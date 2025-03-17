2de2e3216afc86efda32bb7a663cd6c5880fbd75
manzitresor <manzitre@gmail.com>
2025-03-17 23:55:20 +0200

    chore: add auth provider

---
+++ src/authProvider/authProvider.tsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useEffect, useMemo, useState } from 'react'
+import axios from 'axios'
+
+interface AuthContextType {
+  token: string | null
+  setToken: (token: string | null) => void
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined)
+
+export default function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [token, setToken_] = useState<string | null>(localStorage.getItem('token'))
+
+  function setToken(newToken: string | null) {
+    setToken_(newToken)
+  }
+
+  useEffect(() => {
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
+      localStorage.setItem('token', token)
+    } else {
+      delete axios.defaults.headers.common['Authorization']
+      localStorage.removeItem('token')
+    }
+  }, [token])
+
+  const contextValue = useMemo(() => ({ token, setToken }), [token])
+
+  return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+}
+
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return context
+}
