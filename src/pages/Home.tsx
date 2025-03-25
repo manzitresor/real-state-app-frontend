2c758eb71644649bd3bc48eb7bb4082a2d3f7c46
manzitresor <manzitre@gmail.com>
2025-03-25 23:35:42 +0200

    chore: add hero section content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
 import Navbar from '../components/Navbar'
-
+import heroImg from '../assets/heroImg.svg'
 export default function Home() {
   return (
-    <div>
+    <main>
       <Navbar></Navbar>
-    </div>
+      <section className="grid grid-cols-12 h-screen">
+        <div className="col-span-6  py-14 px-20 space-y-4">
+          <h1 className='font-bold text-4xl'>Buy, rent, or sell your property easily</h1>
+          <p className='text-xl text-gray-800'>A great platform to buy, sell, or even rent your properties without any commisions.</p>
+        </div>
+        <div className="col-span-6 h-[36rem] relative">
+          <img src={heroImg} alt="hero-section image" className='w-full h-full '/>
+        </div>
+      </section>
+    </main>
   )
 }
--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -22,7 +22,7 @@ export default function Splash() {
               to="/home"
               className="bg-green-primary-400 rounded-lg px-10 py-4 text-lg font-bold items-center"
             >
-              let's start
+              Let's start
             </Link>
           </div>
         </div>
