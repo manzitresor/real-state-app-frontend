46f39c2f61d76699e53ae4967e30a8349c2a88e2
manzitresor <manzitre@gmail.com>
2025-04-09 18:41:01 +0200

    chore: create property interface

--- src/types/interface.ts
+++ src/types/interface.ts
@@ -5,3 +5,14 @@ export interface StatisticProps {
     title: string,
     description: string,
 }
+
+export interface PropertyList {
+    id: number,
+    price: number,
+    image: string,
+    name: string,
+    description: string,
+    beds: number,
+    bathRooms: number,
+    size: string
+}
