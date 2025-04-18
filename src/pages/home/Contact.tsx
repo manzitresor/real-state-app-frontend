19be8588c60ae9c5bab88d0dcf0363d63ff49009
manzitresor <manzitre@gmail.com>
2025-04-18 15:54:25 +0200

    chore: add Contact section content

---
+++ src/pages/home/Contact.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <section className="bg-white h-full">
+      <h1>Are you a landlord?</h1>
+      <p>Discover ways to increase your home's value and get listed. No Spam.</p>
+      <div>
+        <input type="text" placeholder="Enter your email address" />
+        <button>Submit</button>
+      </div>
+      <p>Join 10,000+ other landlords in our estatery community.</p>
+    </section>
+  )
+}
